bff6546f7fcd9b4c924f6114c4010124092cebcc
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-11-17 00:32:03 -0800

    add basic index for monorepos

---
+++ src/build-monorepo-index.js
@@ -0,0 +1,35 @@
+const shell = require('shelljs');
+const fs = require('fs')
+const path = require('path')
+
+module.exports = function buildMonorepoIndex(packages, outputDir) {
+  const packageRows = packages.map(package => `
+    <div class="package-row">
+      <a href="${path.join(package.name, 'index.html')}">${package.name}</a>
+      <span>${package.description}</span>
+    </div>
+  `)
+  const index = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Page Title</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" type="text/css" href="mono-repo-index.css">
+    </head>
+    <body>
+      <img class="banner" src="storybook.svg" alt="Storybook"/>
+      <div class="content">
+        ${packageRows.join('')}
+      </div>
+    </body>
+    </html>
+  `
+
+  console.log(`=> Building index.html for monorepo`);
+  fs.writeFileSync(path.join(outputDir, 'index.html'), index);
+  shell.cp(path.join(__dirname, 'storybook.svg'), path.join(outputDir, 'storybook.svg'));
+  shell.cp(path.join(__dirname, 'mono-repo-index.css'), path.join(outputDir, 'mono-repo-index.css'));
+}
