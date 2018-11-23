139b64078f557032d8ed5830a5b4113d45529094
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-11-23 13:44:22 -0800

    change page title

--- src/build-monorepo-index.js
+++ src/build-monorepo-index.js
@@ -21,7 +21,7 @@ function generateHTML(packages) {
     <head>
       <meta charset="utf-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <title>Page Title</title>
+      <title>Storybooks</title>
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" type="text/css" href="monorepo-index.css">
     </head>
