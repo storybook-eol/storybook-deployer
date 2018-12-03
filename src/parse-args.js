ba3a6fb869cfd0dd59c14d0800b54ce271c53799
Andrew Lisowski <lisowski54@gmail.com>
2018-12-03 10:56:08 -0800

    take advantage of yargs

--- src/parse-args.js
+++ src/parse-args.js
@@ -1,10 +1,82 @@
-const argv = require('yargs').argv;
+const yargs = require('yargs');
 
 const defaultConfig = {
   gitUsername: 'GH Pages Bot',
   gitEmail: 'hello@ghbot.com',
   commitMessage: 'Deploy Storybook to GitHub Pages'
 };
+
+const argv = yargs
+  .wrap(yargs.terminalWidth())
+  .option('help', {
+    alias: 'h',
+    desc: 'Show help.',
+    type: 'boolean'
+  })
+  .option('existing-output-dir', {
+    alias: 'e',
+    desc:
+      'If you have previously built your storybook output (through a different CI step, etc) and just need to publish it',
+    type: 'string'
+  })
+  .option('out', {
+    alias: 'o',
+    desc: 'Configure the output directory',
+    type: 'string'
+  })
+  .option('packages', {
+    alias: 'p',
+    desc: 'Directory for package.jsons (monorepo support)',
+    type: 'string'
+  })
+  .option('monorepo-index-generator', {
+    alias: 'm',
+    desc:
+      'Path to file to customize the monorepo index.html. This function should return the html for the page.',
+    type: 'string'
+  })
+  .option('script', {
+    alias: 's',
+    desc: 'Specify the build script in your package.json',
+    type: 'string',
+    defaultValue: 'build-storybook'
+  })
+  .option('ci', {
+    desc: 'Deploy the storybook in CI mode (github only)',
+    type: 'boolean'
+  })
+  // Github Variables
+  .option('remote', {
+    desc: 'Git remote to push to',
+    type: 'string',
+    default: 'origin'
+  })
+  .option('branch', {
+    desc: 'Git branch to push to',
+    type: 'string',
+    default: 'gh-pages'
+  })
+  .option('source-branch', {
+    desc: 'Source branch to push from',
+    type: 'string',
+    default: 'master'
+  })
+  .option('host-token-env-variable', {
+    alias: 't',
+    desc: 'Github token for CI publish',
+    type: 'string',
+    default: 'GH_TOKEN'
+  })
+  // AWS Variables
+  .option('aws-profile', {
+    desc: 'AWS profile to use for publishing',
+    type: 'string',
+    default: 'default'
+  })
+  .option('bucket-path', {
+    desc: 'AWS bucket path to use for publishing',
+    type: 'string'
+  }).argv;
 
 module.exports = packageJson => {
   const HOST_TOKEN_ENV_VARIABLE = argv['host-token-env-variable'] || 'GH_TOKEN';
